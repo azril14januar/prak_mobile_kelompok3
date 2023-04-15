26263a314b3302763badf1d38ce8be24890bd2ab
diniantika0309 <diniantika0309@gmail.com>
2023-04-15 12:04:37 +0700

    edit file news fragment

--- lib/modules/home_screen/fragments/news_fragment/news_fragment.dart
+++ lib/modules/home_screen/fragments/news_fragment/news_fragment.dart
@@ -1,13 +1,50 @@
 import 'package:flutter/material.dart';
 
+import '../home_fragment/home_fragment_widgets.dart';
+
 class NewsFragment extends StatelessWidget {
   const NewsFragment({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("News Fragment"),
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    textInputAction: TextInputAction.search,
+                    decoration: InputDecoration(
+                      isDense: true,
+                      hintText: "Masukkan kata kunci disini",
+                      filled: true,
+                      fillColor: Colors.grey[200],
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      suffixIcon: const Icon(
+                        Icons.search_rounded,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                ),
+                child: HomeFragmentWidget.latestNewsSection(
+                  size,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
