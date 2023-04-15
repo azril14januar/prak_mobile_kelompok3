c8cc82eea8fa4f0124d02b3b070754768a6f994d
diniantika0309 <diniantika0309@gmail.com>
2023-04-15 12:28:11 +0700

    edit news

--- lib/models/news.dart
+++ lib/models/news.dart
@@ -14,22 +14,38 @@ class News {
 
 var newsList = [
   News(
-    id: 1, 
-    likeCount: 100000, 
-    title: "[Exclusive] 'Deleting Shinnara, is it possible?'... Limitations and alternatives to boycott in the music industry,", 
-    author: "Kim Jojo", 
-    banner: "https://dispatch.cdnser.be/cms-content/uploads/2023/04/05/e3a345e5-8758-48f4-963e-b29017d0cb5a.png", 
-    date: "2023.04.10 05:09 PM", 
-    desc: "[Dispatch=Reporter Min Ji Gu] If you go to Shin Nara, there is Ive. If you go to Shinnara, there is Jisoo. If you go to Shinnara, there are New Jeans, NCT Dojae, and Seventeen. 'Starship declared a boycott of Shin Nara (record)' for the first time in the music industry. WM, IST, Pledis, and SM also excluded Synnara from the announcement of new album pre-orders. However, Shinnara's album list remains the same. Nothing has changed. Is it the 'unplay' (media play) of the music agency? Or is it a structural problem in music distribution? 'Dispatch' looked into the Shinnara controversy. First of all, I made a call to 18 agencies that were scheduled to make a comeback between April and June. The problems, limitations, and alternatives thought by officials in the music industry were solved through 'Q&D'.",
+    id: 1,
+    likeCount: 100000,
+    title:
+        "[Exclusive] 'Deleting Shinnara, is it possible?'... Limitations and alternatives to boycott in the music industry,",
+    author: "Kim Jojo",
+    banner:
+        "https://dispatch.cdnser.be/cms-content/uploads/2023/04/05/e3a345e5-8758-48f4-963e-b29017d0cb5a.png",
+    date: "2023.04.10 05:09 PM",
+    desc:
+        "[Dispatch=Reporter Min Ji Gu] If you go to Shin Nara, there is Ive. If you go to Shinnara, there is Jisoo. If you go to Shinnara, there are New Jeans, NCT Dojae, and Seventeen. 'Starship declared a boycott of Shin Nara (record)' for the first time in the music industry. WM, IST, Pledis, and SM also excluded Synnara from the announcement of new album pre-orders. However, Shinnara's album list remains the same. Nothing has changed. Is it the 'unplay' (media play) of the music agency? Or is it a structural problem in music distribution? 'Dispatch' looked into the Shinnara controversy. First of all, I made a call to 18 agencies that were scheduled to make a comeback between April and June. The problems, limitations, and alternatives thought by officials in the music industry were solved through 'Q&D'.",
   ),
   News(
-    id: 2, 
-    likeCount: 100000, 
-    title: "Penomeco, 'Rorschach' track list... 'Zico, double title song support fire'", 
-    author: "Hong Jiso", 
-    banner: "https://dispatch.cdnser.be/cms-content/uploads/2023/04/05/e3a345e5-8758-48f4-963e-b29017d0cb5a.png", 
-    date: "2023.04.14 05:07 PM", 
-    desc: "[Dispatch=Reporter Soo-Min Song] Rapper Penomeco has launched a new album spoiler. On the 14th, P Nation released the track list and sampler video of Penomeco's new album 'Penomeco Rorschach Part1' on their official SNS. According to the track list, the new album contains a total of 7 songs. First, the double title songs 'Linda Man' and 'Pyong'. Singer Zico started shooting support for featuring. In addition, 'BOYPABLO', 'Margiela', 'Bubble', 'Around', and 'Trust Me' are included.A new album sampler was also opened. Starting with the pre-released 'Boy Pablo', songs flowed. It captivated the ears with its sensuous yet unique charm. Meanwhile, Penomeco will release 'Penomeco Rorschach Part 1' (on various music sites at 6 pm on the 19th.  ",
+    id: 2,
+    likeCount: 100000,
+    title:
+        "Penomeco, 'Rorschach' track list... 'Zico, double title song support fire'",
+    author: "Hong Jiso",
+    banner:
+        "https://dispatch.cdnser.be/cms-content/uploads/2023/04/05/e3a345e5-8758-48f4-963e-b29017d0cb5a.png",
+    date: "2023.04.14 05:07 PM",
+    desc:
+        "[Dispatch=Reporter Soo-Min Song] Rapper Penomeco has launched a new album spoiler. On the 14th, P Nation released the track list and sampler video of Penomeco's new album 'Penomeco Rorschach Part1' on their official SNS. According to the track list, the new album contains a total of 7 songs. First, the double title songs 'Linda Man' and 'Pyong'. Singer Zico started shooting support for featuring. In addition, 'BOYPABLO', 'Margiela', 'Bubble', 'Around', and 'Trust Me' are included.A new album sampler was also opened. Starting with the pre-released 'Boy Pablo', songs flowed. It captivated the ears with its sensuous yet unique charm. Meanwhile, Penomeco will release 'Penomeco Rorschach Part 1' (on various music sites at 6 pm on the 19th.  ",
   ),
-
+  News(
+    id: 3,
+    likeCount: 110000,
+    title: "BLACKPINK Jennie 'The Idol' Gets An Invitation To Cannes 2023 ",
+    author: "Una lee",
+    banner:
+        "https://dispatch.cdnser.be/cms-content/uploads/2023/04/14/c730c24c-77e5-45a4-94d2-2695614cff73.jpg",
+    date: "2023.04.14 04:49 PM",
+    desc:
+        "[Dispatch=Reporter Soo-Min Song] Rapper Penomeco has launched a new album spoiler. On the 14th, P Nation released the track list and sampler video of Penomeco's new album 'Penomeco Rorschach Part1' on their official SNS. According to the track list, the new album contains a total of 7 songs. First, the double title songs 'Linda Man' and 'Pyong'. Singer Zico started shooting support for featuring. In addition, 'BOYPABLO', 'Margiela', 'Bubble', 'Around', and 'Trust Me' are included.A new album sampler was also opened. Starting with the pre-released 'Boy Pablo', songs flowed. It captivated the ears with its sensuous yet unique charm. Meanwhile, Penomeco will release 'Penomeco Rorschach Part 1' (on various music sites at 6 pm on the 19th.  ",
+  ),
 ];
